4eb702bde62bd07fea86d9604882ff671d793d15
Lukasz Dusza <lukaszdusza280@gmail.com>
2019-08-27 20:54:05 +0200

    dodanie drugiego komentarza do klasy Main w celu wywolania konfliktu

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -2,17 +2,9 @@ package app;
 
 public class Main {
 
-    /**
-     * Aby uruchomić projekt musisz przejść w Intellij: Open --> znaleźć ściężkę do projektu --> zaznaczyć folder pass-braker.
-     * W prawym dolnym ekranie pojawi się popup z informacją o "eneble auto import" --> kliknij na to.
-     * Jeśli nie wyskoczy ten popup, to: prawy klawisz myszy na plik pom.xml --> Maven --> Reimport.
-     * W klasie Main poniżej możesz mieć na czerwono RandomStringUtils. Kliknij na tym i wciśnij Alt + Enter i zaznacz import.
-     * Nie masz import? to powtórz trzecią gwiazdkę :)
-     * Uruchom zielonym buttonem.
-     * ENJOY!
-     * */
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("another Hello");
     }
 }
